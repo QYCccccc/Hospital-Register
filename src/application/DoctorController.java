c1917e73ba20379d582643e2568308f077c4459a
QYCccccc <qyc@hust.edu.cn>
2019-04-17 23:30:08 +0800

    starting doctor view

--- src/application/DoctorController.java
+++ src/application/DoctorController.java
@@ -1,30 +1,89 @@
 package application;
 
 import com.sun.javafx.robot.impl.FXRobotHelper;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 
 public class DoctorController implements Initializable {
+
+    public static final class Register {
+        private SimpleStringProperty number;
+        private SimpleStringProperty patientName;
+        private SimpleStringProperty regTime;
+        private SimpleStringProperty regType;
+
+        Register(String number, String patientName, String regTime, String regType) {
+            this.number = new SimpleStringProperty(number);
+            this.patientName = new SimpleStringProperty(patientName);
+            this.regTime = new SimpleStringProperty(regTime);
+            this.regType = new SimpleStringProperty(regType);
+        }
+    }
+
+    public static final class Income {
+        private SimpleStringProperty deptName;
+        private SimpleStringProperty doctId;
+        private SimpleStringProperty doctName;
+        private SimpleStringProperty regType;
+        private SimpleStringProperty regCount;
+        private SimpleStringProperty totalIncome;
+
+        public Income(String deptName, String doctId, String doctName,
+                      String regType, String regCount, String totalIncome) {
+            this.deptName = new SimpleStringProperty(deptName);
+            this.doctId = new SimpleStringProperty(doctId);
+            this.doctName = new SimpleStringProperty(doctName);
+            this.regType = new SimpleStringProperty(regType);
+            this.regCount = new SimpleStringProperty(regCount);
+            this.totalIncome = new SimpleStringProperty(totalIncome);
+        }
+    }
     public static String name;
     @FXML
     private Label labelInfo;
     @FXML
-    private Button buttonExit;
+    private Button buttonExit,buttonRefresh;
+    @FXML
+    private TableView table_reg, table_income;
+    @FXML
+    private TableColumn<Register, String> reg_id,
+            patient_name,
+            reg_time,
+            regTab_reg_type;
+    @FXML
+    private TableColumn<Income, String> dept_name,
+            doct_id,
+            doct_name,
+            incomeTab_reg_type,
+            reg_count,
+            totalincome;
+    @FXML
+    private Tab tabReg, tabIncome;
+    @FXML
+    private TabPane tabPane;
+
+    private ObservableList<Register> listReg = FXCollections.observableArrayList();
+    private ObservableList<Income> listIncome = FXCollections.observableArrayList();
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         FXRobotHelper.getStages().get(0).setTitle("");
-        labelInfo.setText(name);
+        labelInfo.setText("欢迎, " + name);
+
         buttonExit.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
@@ -49,5 +108,26 @@ public class DoctorController implements Initializable {
                 FXRobotHelper.getStages().get(0).setScene(scene);
             }
         });
+
+        //将register对象的某些属性bind到挂号列表的特定列
+        reg_id.setCellValueFactory(cellData -> cellData.getValue().number);
+        patient_name.setCellValueFactory(cellData -> cellData.getValue().patientName);
+        reg_time.setCellValueFactory(cellData -> cellData.getValue().regTime);
+        regTab_reg_type.setCellValueFactory(cellData -> cellData.getValue().regType);
+
+        //将Income对象的某些属性bind到收入列表的特定列
+        dept_name.setCellValueFactory(cellData -> cellData.getValue().deptName);
+        doct_id.setCellValueFactory(cellData -> cellData.getValue().doctId);
+        doct_name.setCellValueFactory(cellData -> cellData.getValue().doctName);
+        incomeTab_reg_type.setCellValueFactory(cellData -> cellData.getValue().regType);
+        reg_count.setCellValueFactory(cellData -> cellData.getValue().regCount);
+        totalincome.setCellValueFactory(cellData -> cellData.getValue().totalIncome);
+    }
+    //更新按钮的事件处理函数
+    private void on_push_buttonRefresh() {
+        if (tabPane.getSelectionModel().getSelectedItem() == tabReg) {
+            ResultSet result;
+
+        }
     }
 }
